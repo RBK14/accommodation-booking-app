fba9ffae3744f57f285ffde57d234ce02f805e90
RBK14 <maciek.rybka2004@gmail.com>
2025-10-30 13:40:12 +0100

    structure fix

--- accommodation-booking-api/AccommodationBooking.Application/Users/Commands/UpdateEmail/UpdateEmailCommand.cs
+++
@@ -1,9 +0,0 @@
-﻿using ErrorOr;
-using MediatR;
-
-namespace AccommodationBooking.Application.Users.Commands.UpdateEmail
-{
-    public record UpdateEmailCommand(
-        Guid UserId,
-        string Email) : IRequest<ErrorOr<Unit>>;
-}
