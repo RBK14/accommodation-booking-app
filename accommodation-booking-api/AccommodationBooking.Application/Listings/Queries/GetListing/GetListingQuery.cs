13ae20adbf1dfc52092e2932aa607c290a580cef
RBK14 <maciek.rybka2004@gmail.com>
2025-11-13 00:19:03 +0100

    add GetListing query

---
+++ accommodation-booking-api/AccommodationBooking.Application/Listings/Queries/GetListing/GetListingQuery.cs
@@ -0,0 +1,8 @@
+﻿using AccommodationBooking.Domain.ListingAggregate;
+using ErrorOr;
+using MediatR;
+
+namespace AccommodationBooking.Application.Listings.Queries.GetListing
+{
+    public record GetListingQuery(Guid ListingId) : IRequest<ErrorOr<Listing>>;
+}
---
+++ accommodation-booking-api/AccommodationBooking.Application/Listings/Queries/GetListing/GetListingQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using AccommodationBooking.Application.Common.Intrefaces.Persistence;
+using AccommodationBooking.Domain.Common.Errors;
+using AccommodationBooking.Domain.ListingAggregate;
+using ErrorOr;
+using MediatR;
+
+namespace AccommodationBooking.Application.Listings.Queries.GetListing
+{
+    public class GetListingQueryHandler(IUnitOfWork unitOfWork) : IRequestHandler<GetListingQuery, ErrorOr<Listing>>
+    {
+        private readonly IUnitOfWork _unitOfWork = unitOfWork;
+
+        public async Task<ErrorOr<Listing>> Handle(GetListingQuery query, CancellationToken cancellationToken)
+        {
+            if (await _unitOfWork.Listings.GetByIdAsync(query.ListingId) is not Listing listing)
+                return Errors.Listing.NotFound;
+
+            return listing;
+        }
+    }
+}
