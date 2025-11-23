67da3ec30f31f5f7b34c672ac492161d26d873d2
RBK14 <maciek.rybka2004@gmail.com>
2025-11-23 16:38:57 +0100

    add UpdateStatus command

---
+++ accommodation-booking-api/AccommodationBooking.Application/Reservations/Commands/UpdateStatus/UpdateStatusCommandHandler.cs
@@ -0,0 +1,66 @@
+﻿using AccommodationBooking.Application.Common.Intrefaces.Persistence;
+using AccommodationBooking.Domain.Common.Errors;
+using AccommodationBooking.Domain.Common.Exceptions;
+using AccommodationBooking.Domain.ReservationAggregate;
+using AccommodationBooking.Domain.ReservationAggregate.Enums;
+using ErrorOr;
+using MediatR;
+
+namespace AccommodationBooking.Application.Reservations.Commands.UpdateStatus
+{
+    public class UpdateStatusCommandHandler(IUnitOfWork unitOfWork) : IRequestHandler<UpdateStatusCommand, ErrorOr<Unit>>
+    {
+        private readonly IUnitOfWork _unitOfWork = unitOfWork;
+
+        public async Task<ErrorOr<Unit>> Handle(UpdateStatusCommand command, CancellationToken cancellationToken)
+        {
+            if (await _unitOfWork.Reservations.GetByIdAsync(command.ReservationId) is not Reservation reservation)
+                return Errors.Reservation.NotFound;
+
+            var newStatus = ReservationStatusExtensions.Parse(command.Status);
+
+            try
+            {
+                switch (newStatus)
+                {
+                    case ReservationStatus.InProgress:
+                        reservation.MarkAsInProgress();
+                        break;
+
+                    case ReservationStatus.Completed:
+                        reservation.MarkAsCompleted();
+                        break;
+
+                    case ReservationStatus.Cancelled:
+                        reservation.Cancel();
+                        break;
+
+                    case ReservationStatus.NoShow:
+                        reservation.MarkAsNoShow();
+                        break;
+
+                    case ReservationStatus.Accepted:
+                        return Error.Validation(
+                            "Reservation.InvalidTransition",
+                            "Cannot manually transition to 'Accepted' status.");
+
+                }
+
+                await _unitOfWork.CommitAsync(cancellationToken);
+            }
+            // TODO: Przenieść Errors do Domain
+            catch (DomainIllegalStateException ex)
+            {
+                await _unitOfWork.RollbackAsync(cancellationToken);
+                return Error.Conflict("Reservation.IllegalState", ex.Message);
+            }
+            catch (Exception ex)
+            {
+                await _unitOfWork.RollbackAsync(cancellationToken);
+                return Error.Failure("Reservation.UpdateFailed", ex.Message);
+            }
+
+            return Unit.Value;
+        }
+    }
+}
---
+++ accommodation-booking-api/AccommodationBooking.Application/Reservations/Commands/UpdateStatus/UpdateStatusCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using AccommodationBooking.Domain.ListingAggregate.Enums;
+using AccommodationBooking.Domain.ReservationAggregate.Enums;
+using FluentValidation;
+
+namespace AccommodationBooking.Application.Reservations.Commands.UpdateStatus
+{
+    public class UpdateStatusCommandValidator : AbstractValidator<UpdateStatusCommand>
+    {
+        public UpdateStatusCommandValidator() 
+        {
+            RuleFor(x => x.Status)
+                .NotEmpty().WithMessage("Status jest wymagany.")
+                .Must(ReservationStatusExtensions.IsValidReservationStatus)
+                .WithMessage("Nieprawidłowy status");
+        }
+    }
+}
