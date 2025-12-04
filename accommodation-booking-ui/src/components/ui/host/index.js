bb13cfb9a51a567fa8bc8a24ccd014e603b83af6
RBK14 <maciek.rybka2004@gmail.com>
2025-12-04 21:52:38 +0100

    structure fix

---
+++ accommodation-booking-ui/src/components/ui/host/index.js
@@ -0,0 +1,4 @@
+export { default as OfferDetailsSection } from './OfferDetailsSection';
+export { default as OfferGallerySection } from './OfferGallerySection';
+export { default as ReservationsSection } from './ReservationsSection';
+export { default as ReviewsSection } from './ReviewsSection';
