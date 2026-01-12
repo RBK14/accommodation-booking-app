1524082913b8d7b9f7c32c9abf3a1945cf72ce7b
RBK14 <maciek.rybka2004@gmail.com>
2026-01-12 21:56:19 +0100

    add unit tests

---
+++ accommodation-booking-api/tests/AccommodationBooking.Application.UnitTests/CreateReservation/CreateReservationCommandValidatorTests.cs
@@ -0,0 +1,77 @@
+﻿using AccommodationBooking.Application.Reservations.Commands.CreateReservation;
+using FluentValidation.TestHelper;
+
+namespace AccommodationBooking.Application.UnitTests.CreateReservation
+{
+    public class CreateReservationCommandValidatorTests
+    {
+        private readonly CreateReservationCommandValidator _validator;
+
+        public CreateReservationCommandValidatorTests()
+        {
+            _validator = new CreateReservationCommandValidator();
+        }
+
+        [Fact]
+        public void Validate_Should_HaveNoError_When_DatesAreCorrect()
+        {
+            var command = new CreateReservationCommand(
+                Guid.NewGuid(),
+                Guid.NewGuid(),
+                DateOnly.FromDateTime(DateTime.Now),
+                DateOnly.FromDateTime(DateTime.Now.AddDays(1)));
+
+            var result = _validator.TestValidate(command);
+
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+
+        [Fact]
+        public void Validate_Should_Fail_When_CheckOutIsBeforeCheckIn()
+        {
+            var today = DateOnly.FromDateTime(DateTime.Now);
+            var yesterday = today.AddDays(-1);
+
+            var command = new CreateReservationCommand(
+                Guid.NewGuid(),
+                Guid.NewGuid(),
+                CheckIn: today,
+                CheckOut: yesterday);
+
+            _validator.TestValidate(command)
+                .ShouldHaveValidationErrorFor(x => x.CheckOut)
+                .WithErrorMessage("Data wymeldowania musi być późniejsza niż data zameldowania.");
+        }
+
+        [Fact]
+        public void Validate_Should_Fail_When_CheckOutIsSameAsCheckIn()
+        {
+            var today = DateOnly.FromDateTime(DateTime.Now);
+
+            var command = new CreateReservationCommand(
+                Guid.NewGuid(),
+                Guid.NewGuid(),
+                CheckIn: today,
+                CheckOut: today);
+
+            _validator.TestValidate(command)
+                .ShouldHaveValidationErrorFor(x => x.CheckOut);
+        }
+
+        [Fact]
+        public void Validate_Should_Fail_When_DatesAreEmpty()
+        {
+            var command = new CreateReservationCommand(
+                Guid.NewGuid(),
+                Guid.NewGuid(),
+                CheckIn: default,
+                CheckOut: default);
+
+            _validator.TestValidate(command)
+                .ShouldHaveValidationErrorFor(x => x.CheckIn);
+
+            _validator.TestValidate(command)
+                .ShouldHaveValidationErrorFor(x => x.CheckOut);
+        }
+    }
+}
---
+++ accommodation-booking-api/tests/AccommodationBooking.Domain.UnitTests/GuestProfileTests.cs
@@ -0,0 +1,66 @@
+﻿using AccommodationBooking.Domain.Common.Exceptions;
+using AccommodationBooking.Domain.GuestProfileAggregate;
+using FluentAssertions;
+
+namespace AccommodationBooking.Domain.UnitTests
+{
+    public class GuestProfileTests
+    {
+        [Fact]
+        public void Create_Should_ReturnGuestProfile_When_UserIdIsValid()
+        {
+            var userId = Guid.NewGuid();
+
+            var guestProfile = GuestProfile.Create(userId);
+
+            guestProfile.Should().NotBeNull();
+            guestProfile.UserId.Should().Be(userId);
+            guestProfile.ReservationIds.Should().BeEmpty();
+        }
+
+        [Fact]
+        public void Create_Should_ThrowException_When_UserIdIsEmpty()
+        {
+            Action act = () => GuestProfile.Create(Guid.Empty);
+
+            act.Should().Throw<DomainValidationException>()
+               .WithMessage("GuestProfile must be associated with a valid UserId.");
+        }
+
+        [Fact]
+        public void AddReservationId_Should_AddId_When_ReservationIdIsNew()
+        {
+            var guestProfile = GuestProfile.Create(Guid.NewGuid());
+            var reservationId = Guid.NewGuid();
+
+            guestProfile.AddReservationId(reservationId);
+
+            guestProfile.ReservationIds.Should().Contain(reservationId);
+        }
+
+        [Fact]
+        public void AddReservationId_Should_ThrowException_When_ReservationIdIsDuplicate()
+        {
+            var guestProfile = GuestProfile.Create(Guid.NewGuid());
+            var reservationId = Guid.NewGuid();
+            guestProfile.AddReservationId(reservationId);
+
+            Action act = () => guestProfile.AddReservationId(reservationId);
+
+            act.Should().Throw<DomainIllegalStateException>()
+               .WithMessage("The reservation is already associated with this guest.");
+        }
+
+        [Fact]
+        public void RemoveReservationId_Should_RemoveId_When_ReservationIdExists()
+        {
+            var guestProfile = GuestProfile.Create(Guid.NewGuid());
+            var reservationId = Guid.NewGuid();
+            guestProfile.AddReservationId(reservationId);
+
+            guestProfile.RemoveReservationId(reservationId);
+
+            guestProfile.ReservationIds.Should().BeEmpty();
+        }
+    }
+}
